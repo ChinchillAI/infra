e13595648e28c81abdc4ff5320f2adbc1f7f186c
ChinchillAI <147962344+ChinchillAI@users.noreply.github.com>
2024-01-24 11:52:40 -0800

    feat(makoto): add Firworks pubkey
    Adds Fir to makoto authorized users.

--- hosts/makoto/configuration.nix
+++ hosts/makoto/configuration.nix
@@ -40,6 +40,7 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDhzSb0t3XxmRznThUb7EOW51yFQULht02frD+7/lQDeouqjxxpiPhVCH0bEQdtX2lcUyEnK52RusAoXBYnTvjcgaLc9kypDY82UMndrqAFPk434y3SslEbJCubU/iOJ+z9g3mOJH8v02mXOIINhuvkzQyaekkZ3xR7m/yemPfwfx99TqnYUD/yuIrIn3gQcsHAdWtgk+vTrH2q8uGkcZyENaRkM9uS2OEr688KBUA+QnpT/9GGdR0y+saqMFryqFL0TE7Lemesoir+1cV0ibBb5gGCQr2ZppmDJ6f2CB2AHcd/9XXIy5n98zFdSUUr/kl02L62lQt5zzm+8Z7iEE7+82ea+RgBQy+QuTSFmjD+MhI6Y+zP+MiQmugUkGfCfyosD5PZgqlxuWCPKT7sNFjS8TK8lYbwc8kvqoTe+Ve10GT6Hq3My6eoMiJi4996ZjboyAZ064xXhIzan97viY+KYEZqrJOnfU98aTfmhbc0l2HbYp3decZsbAOwFb85ZKdDNagheIDeCtjHr9L8sG0FFhO6JdeUuUm17w9AVtBgJ0X83PLxmrx+Y+WdMx646/PYs6340/cQkXCy45iORPicWD5pqCciHYDzukYeZ+PbrIHoiTWYYIfnsXM3piE8c4tkhvfsbxJpdo9YTq/hXNbQCvvjqyoNOEMuxMbWJhBNZQ== chinchillAI"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEz5kBF9xzg6fej1zjgk29c+oRcl4Lv8GSnV8Bt5cAyF Firworks"
   ];
 
   system.stateVersion = "23.11";
