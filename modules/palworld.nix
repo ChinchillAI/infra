4924fcf24d8fcdb9b6c945e1f41eb2cb9ee70763
ChinchillAI <147962344+ChinchillAI@users.noreply.github.com>
2024-02-11 02:10:35 -0800

    feat(palworld): automatic upgrades
    Adds another systemd service to handle Palworld releases. Monitors the
    current public build ID and automatically stops palworld, upgrades, and
    restarts when it changes.  Fires every hour for now.

--- modules/palworld.nix
+++ modules/palworld.nix
@@ -90,8 +90,45 @@ in
       };
     };
 
-    palworld-restart = {
+    palworld-upgrade = {
       startAt = "hourly";
+      serviceConfig = {
+        ExecStart = "${pkgs.resholve.writeScript "upgrade" {
+	      interpreter = "${pkgs.zsh}/bin/zsh";
+	      inputs = with pkgs; [ rcon coreutils systemd curl jq ];
+	      execer = with pkgs; [ "cannot:${rcon}/bin/rcon" "cannot:${systemd}/bin/systemctl" "cannot:${curl}/bin/curl" "cannot:${jq}/bin/jq"];
+	    } ''
+	     set -x
+	     CURRENT_PALWORLD_VERSION=$(curl -X GET 'https://api.steamcmd.net/v1/info/2394010' -s | jq '.data.["2394010"].depots.branches.public.buildid' -r )
+	     INSTALLED_PALWORLD_VERSION=$(cat /var/lib/palworld/installed_palworld_version)
+	     if [[ "$CURRENT_PALWORLD_VERSION" != "$INSTALLED_PALWORLD_VERSION"]]; then
+	       echo "New Palworld version detected! Starting upgrade countdown..."
+	       rcon -s palworld -n broadcast New_version_released
+	       rcon -s palworld -n broadcast Upgrade_in_15_minutes
+	       sleep 300
+	       rcon -s palworld -n broadcast Upgrade_in_10_minutes
+	       sleep 300
+	       rcon -s palworld -n broadcast Upgrade_in_5_minutes
+	       sleep 240
+	       rcon -s palworld -n broadcast Upgrade_in_1_minute
+	       sleep 40
+	       echo "Starting Palworld upgrade..."
+	       systemctl stop palworld.service
+	       systemctl start steam@2394010.service
+	       echo "Palworld upgrade complete! Restarting server..."
+	       systemctl start palworld.service
+	       echo "Palworld restart complete."
+	       echo $CURRENT_PALWORLD_VERSION > /var/lib/palworld/installed_palworld_version
+	     else
+	       echo "Palworld already up to date."
+	     fi
+	    ''}";
+      };
+    };
+
+    palworld-restart = {
+      startAt = "05:30:00"; # daily, 30 minutes prestream
+      #requisite = [ "palworld.service" ];
       serviceConfig = {
         ExecStart = "${pkgs.resholve.writeScript "restart" {
 	      interpreter = "${pkgs.zsh}/bin/zsh";
@@ -99,6 +136,7 @@ in
 	      execer = with pkgs; [ "cannot:${rcon}/bin/rcon" "cannot:${systemd}/bin/systemctl" ];
 	    } ''
 	     set -x
+	     echo "Starting daily Palworld restart countdown..."
 	     rcon -s palworld -n broadcast Restart_in_15_minutes
 	     sleep 300
 	     rcon -s palworld -n broadcast Restart_in_10_minutes
@@ -107,7 +145,9 @@ in
 	     sleep 240
 	     rcon -s palworld -n broadcast Restart_in_1_minute
 	     sleep 40
+	     echo "Restarting Palworld..."
 	     systemctl restart palworld.service
+	     echo "Palworld restart complete."
 	    ''}";
       };
     };
