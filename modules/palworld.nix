cdc9a6432aa22cc73809d5656368b50ddf063f5c
ChinchillAI <147962344+ChinchillAI@users.noreply.github.com>
2024-02-11 00:54:23 -0800

    feat(palworld): testing autorestart timer service
    Adds a second systemd service to automatically restart palworld every
    24-hours. Requires rconrc to be copied to the root home directory;
    replace when secret support added.

--- modules/palworld.nix
+++ modules/palworld.nix
@@ -27,35 +27,37 @@ in
     allowedTCPPorts = [ 8211 ];
   };
 
-  systemd.services.palworld = {
-    wantedBy = [ "multi-user.target" ];
+  systemd.services = {
+    palworld = {
+      wantedBy = [ "multi-user.target" ];
 
-    #wants = [ "steam@${steam-app}.service" ];
-    #after = [ "steam@${steam-app}.service" ];
+      #wants = [ "steam@${steam-app}.service" ];
+      #after = [ "steam@${steam-app}.service" ];
 
-    # Note the following files are currently non-functionally defined:
-    # /var/lib/steam/.rconrc
-    # /var/lib/steam-app-2394010/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini 
+      # Note the following files are currently non-functionally defined:
+      # /var/lib/steam/.rconrc
+      # /root/.rconrc
+      # /var/lib/steam-app-2394010/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini 
 
-    path = with pkgs; [ xdg-user-dirs rcon ];
+      path = with pkgs; [ xdg-user-dirs rcon ];
 
-    serviceConfig = {
-      ExecStart = lib.escapeShellArgs [
-        "/var/lib/steam-app-${steam-app}/PalServer.sh"
-        "-useperfthreads"
-        "-NoAsyncLoadingThread"
-        "-UseMultithreadForDS"
-      ];
-      Nice = "-5";
-      PrivateTmp = true;
-      Restart = "always";
-      User = "steam";
-      WorkingDirectory = "~";
+      serviceConfig = {
+        ExecStart = lib.escapeShellArgs [
+          "/var/lib/steam-app-${steam-app}/PalServer.sh"
+          "-useperfthreads"
+          "-NoAsyncLoadingThread"
+          "-UseMultithreadForDS"
+        ];
+        Nice = "-5";
+        PrivateTmp = true;
+        Restart = "always";
+        User = "steam";
+        WorkingDirectory = "~";
 
-      # Note: rcon returns code 3 with this method for some reason
-      # Not waiting for response works, but we are racing the save
-      # probably replace with mcrcon when we add secret support
-      ExecStop = "${pkgs.resholve.writeScript "shutdown" {
+        # Note: rcon returns code 3 with this method for some reason
+        # Not waiting for response works, but we are racing the save
+        # probably replace with mcrcon when we add secret support
+        ExecStop = "${pkgs.resholve.writeScript "shutdown" {
 	      interpreter = "${pkgs.zsh}/bin/zsh";
 	      inputs = with pkgs; [ rcon coreutils ];
 	      execer = with pkgs; [ "cannot:${rcon}/bin/rcon" ];
@@ -81,10 +83,33 @@ in
 	     rcon -s palworld -n shutdown 1 Shutdown_in_1_seconds
 	     sleep 10
 	    ''}";
+      };
+      environment = {
+        LD_LIBRARY_PATH = "/var/lib/steam-app-${steam-app}/linux64:${pkgs.glibc}/lib";
+        SteamAppId = "892970";
+      };
     };
-    environment = {
-      LD_LIBRARY_PATH = "/var/lib/steam-app-${steam-app}/linux64:${pkgs.glibc}/lib";
-      SteamAppId = "892970";
+
+    palworld-restart = {
+      startAt = "hourly";
+      serviceConfig = {
+        ExecStart = "${pkgs.resholve.writeScript "restart" {
+	      interpreter = "${pkgs.zsh}/bin/zsh";
+	      inputs = with pkgs; [ rcon coreutils systemd ];
+	      execer = with pkgs; [ "cannot:${rcon}/bin/rcon" "cannot:${systemd}/bin/systemctl" ];
+	    } ''
+	     set -x
+	     rcon -s palworld -n broadcast Restart_in_15_minutes
+	     sleep 300
+	     rcon -s palworld -n broadcast Restart_in_10_minutes
+	     sleep 300
+	     rcon -s palworld -n broadcast Restart_in_5_minutes
+	     sleep 240
+	     rcon -s palworld -n broadcast Restart_in_1_minute
+	     sleep 40
+	     systemctl restart palworld.service
+	    ''}";
+      };
     };
   };
 }
